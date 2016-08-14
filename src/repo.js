891e6fe185b8d8b84f2a338de6e2531289d180c5
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-08-14 16:04:07 -0400

    Tests

--- src/repo.js
+++ src/repo.js
@@ -2,12 +2,6 @@ import Microcosm from 'microcosm'
 
 class Repo extends Microcosm {
 
-  constructor(options) {
-    super(options)
-
-    // Place stores here
-  }
-
 }
 
 export default Repo
--- test/helpers/setup-browser-env.js
+++ test/helpers/setup-browser-env.js
@@ -1,3 +1,5 @@
-global.document  = require('jsdom').jsdom('<body></body>');
-global.window    = document.defaultView;
-global.navigator = window.navigator;
+require('babel-register')
+
+global.document  = require('jsdom').jsdom('<body></body>')
+global.window    = document.defaultView
+global.navigator = window.navigator
