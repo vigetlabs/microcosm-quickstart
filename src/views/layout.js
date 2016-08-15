81271d389331605b9fed7b07236d9fa14ae4892c
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-08-14 20:04:03 -0400

    Update layout copy

--- src/views/layout.js
+++ src/views/layout.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-export default function Layout ({ children }) {
+export default function Layout ({children}) {
 
   return (
     <div>
-      <h1>Microcosm Starter</h1>
-      <p>Great things await</p>
-
+      <h1>You've successfully installed microcosm-starter</h1>
+      <p>Great things await!</p>
       {children}
     </div>
   )
