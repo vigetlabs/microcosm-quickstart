eabd5d9ce6426454aff735874df5b299d27716b9
Eli Fatsi <eli.fatsi@viget.com>
2017-05-23 15:38:17 -0600

    Update package versions, clean up index/boot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './style/application.css'
-import boot from './boot'
+import React from 'react'
+import DOM from 'react-dom'
+import Repo from './repo'
+import routes from './routes'
+import Presenter from 'microcosm/addons/presenter'
+import { Router, browserHistory } from 'react-router'
 
 // Do anything on startup here
+const repo = new Repo()
 
 // Then mount the application
-boot(document.getElementById('root'))
+DOM.render((
+  <Presenter repo={repo}>
+    <Router history={browserHistory} routes={routes} />
+  </Presenter>
+), document.getElementById('root'))
